d04f5fdafb3cbfe2112a8e7d40399c2b1465e349
Suso <jesus.martinez@galicia.com>
2020-03-19 21:05:35 +0100

    Lighter reloading
    Now instead of reloading the whole page, the reload button just chooses a new topic. Plus: after the last element, tapping will choose a new topic

--- temario.js
+++ temario.js
@@ -1,22 +1,33 @@
-let temaIndex = Math.floor(Math.random()*temas.length);
-let tema = temas[temaIndex];
-document.title = tema.number + " - " + tema.title;
-let counter = 0;
+let temaIndex, tema, counter;
+var $list = $("#sectionList");
+var $reloadButton = $("#reloadButton");
+$reloadButton.click(function () {
+    newTopic();
+});
+newTopic();
+
+function newTopic() {
+    // Clean the UI
+    $list.empty();
+    // Choose new topic
+    temaIndex = Math.floor(Math.random() * temas.length);
+    tema = temas[temaIndex];
+    document.title = tema.number + " - " + tema.title;
+    counter = 0;
+    $list.append($("<li class=\"list-group-item\">").text(tema.number + " - " + tema.title));
+    nextElement();
+}
+
 function nextElement() {
     let texto = tema.elements[counter].number + ". " + tema.elements[counter].name;
-    var $div = $("<li>", { "class": "list-group-item", text: texto});
+    var $div = $("<li>", { "class": "list-group-item", text: texto });
     $div.click(function () {
         if (counter < tema.elements.length) {
             nextElement();
+        } else {
+            newTopic();
         }
     });
     $list.append($div);
     counter++;
 }
-var $list = $("#sectionList");
-$list.append($("<li class=\"list-group-item\">").text(tema.number + " - " + tema.title));
-nextElement();
-var $reloadButton = $("#reloadButton");
-$reloadButton.click(function () {
-    location.reload();
-});
