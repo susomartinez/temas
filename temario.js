176c2d010246f03cc22ecd9c0f7d93417fc853db
Suso <jesus.martinez@galicia.com>
2020-03-19 20:54:09 +0100

    Reload button
    Added a reload button. Practical!

--- temario.js
+++ temario.js
@@ -1,5 +1,6 @@
 let temaIndex = Math.floor(Math.random()*temas.length);
 let tema = temas[temaIndex];
+document.title = tema.number + " - " + tema.title;
 let counter = 0;
 function nextElement() {
     let texto = tema.elements[counter].number + ". " + tema.elements[counter].name;
@@ -15,3 +16,7 @@ function nextElement() {
 var $list = $("#sectionList");
 $list.append($("<li class=\"list-group-item\">").text(tema.number + " - " + tema.title));
 nextElement();
+var $reloadButton = $("#reloadButton");
+$reloadButton.click(function () {
+    location.reload();
+});
