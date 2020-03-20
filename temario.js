0884be782897bd02b7ba95966f3396f9411ef3dc
Suso <jesus.martinez@galicia.com>
2020-03-20 10:03:41 +0100

    in & out params
    Now the URL accepts in and out parameters to indicate which topics must be banned or left

--- temario.js
+++ temario.js
@@ -1,17 +1,50 @@
 let temaIndex, tema, counter;
+let inTopics = []
+let outTopics = [];
+let availableTopics = [];
+
 var $list = $("#sectionList");
 var $reloadButton = $("#reloadButton");
 $reloadButton.click(function () {
-    newTopic();
+    newTopic(inTopics, outTopics);
 });
-newTopic();
+getPreviousTopics();
+newTopic(inTopics, outTopics);
 
-function newTopic() {
+function getPreviousTopics() {
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('in')) {
+        let ins = urlParams.get('in').split(",");
+        for (str of ins) {
+            inTopics.push(Number.parseInt(str));
+        }
+    }
+    if (urlParams.has('out')) {
+        let outs = urlParams.get('out').split(",");
+        for (str of outs) {
+            outTopics.push(Number.parseInt(str));
+        }
+    }
+}
+
+function newTopic(ins, outs) {
     // Clean the UI
     $list.empty();
-    // Choose new topic
-    temaIndex = Math.floor(Math.random() * temas.length);
-    tema = temas[temaIndex];
+    // Empty availableTopics
+    availableTopics.splice(0);
+    // Take out banned topics
+    if (ins.length == 0) {
+        availableTopics = temas.filter(function (tema) {
+            return !(outs.includes(tema.number));
+        });
+    } else { // Choose only inTopics
+        availableTopics = temas.filter(function (tema) {
+            return ins.includes(tema.number);
+        });
+    }
+    // Choose new topic from the available ones
+    temaIndex = Math.floor(Math.random() * availableTopics.length);
+    tema = availableTopics[temaIndex];
     document.title = tema.number + " - " + tema.title;
     counter = 0;
     $list.append($("<li class=\"list-group-item\">").text(tema.number + " - " + tema.title));
@@ -25,7 +58,7 @@ function nextElement() {
         if (counter < tema.elements.length) {
             nextElement();
         } else {
-            newTopic();
+            newTopic(inTopics, outTopics);
         }
     });
     $list.append($div);
